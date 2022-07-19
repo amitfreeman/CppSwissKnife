594fe5058702ca938603e617b7dde0f09c6a99be
Amit Paropkari <netamitxi@gmail.com>
2022-07-19 11:33:27 +0530

    binary search

--- DS_DP_Problems/BinarySearch.cpp
+++ DS_DP_Problems/BinarySearch.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 class Solution {
-public:
+
     int binary_search(int start, int end, int target, vector<int>& nums){
          if(start < end){
              int mid=(start+end)/2;
@@ -20,9 +21,42 @@ public:
            return -1;
     }
 
+public:
+
     int search(vector<int>& nums, int target) {
         int end=nums.size();
 
         return binary_search(0, end, target, nums);
     }
 };
+
+int main(){
+
+    vector<int> arr={9,6,7,18,-2,100,2003,2022,69,44,101};
+    sort(arr.begin(), arr.end());
+
+    for(int &i: arr)
+       cout<<" "<<i;
+
+    cout<<endl;
+  
+    Solution s;
+    int num=100;
+    int pos=s.search(arr, num);
+    if(pos<0) 
+       cout<<"Element num: "<<num<<", not found";
+    else 
+       cout<<"Found element num: "<<num<<", at postion: "<<pos;
+
+     cout<<endl;     
+
+    num=199;
+    pos=s.search(arr, num);
+    if(pos<0) 
+       cout<<"Element num: "<<num<<", not found";
+    else 
+       cout<<"Found element num: "<<num<<", at postion: "<<pos;
+
+     cout<<endl;     
+
+}
